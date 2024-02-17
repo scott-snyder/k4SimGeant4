731da9667281164b0489a0e02ca6fe717dbcbd89
scott snyder <sss@karma>
2024-02-16 22:34:23 -0500

    Suppress deprecation warning.

--- Detector/DetComponents/src/RedoSegmentation.cpp
+++ Detector/DetComponents/src/RedoSegmentation.cpp
@@ -1,3 +1,5 @@
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
 #include "RedoSegmentation.h"
 
 // DD4hep
--- SimG4Components/src/SimG4SaveTrackerHits.cpp
+++ SimG4Components/src/SimG4SaveTrackerHits.cpp
@@ -1,3 +1,5 @@
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
 #include "SimG4SaveTrackerHits.h"
 
 // k4SimGeant4
