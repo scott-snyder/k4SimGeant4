c362b4cbcb7faf6003a1a25b832d8a4a85a5a959
sss <sss@karma>
2025-08-13 15:01:52 -0400

    Remove non-const methods of GeoSvc.
    Now that the IGeoSvc interface class
    has been updated, we can remove the non-const methods from its
    derived classes (and add override keywords).
    
    Also some modernization / clean up.

--- Detector/DetComponents/src/GeoSvc.h
+++ Detector/DetComponents/src/GeoSvc.h
@@ -14,29 +14,28 @@ class G4VUserDetectorConstruction;
 class GeoSvc : public extends<Service, IGeoSvc> {
 
 public:
-  GeoSvc(const std::string& name, ISvcLocator* svc);
+  using base_class::base_class;
 
-  virtual StatusCode initialize() final;
-  virtual StatusCode finalize() final;
-
-  // This function generates the DD4hep geometry
+  /// Destructor
+  virtual ~GeoSvc();
+  /// Initialize function
+  virtual StatusCode initialize() final override;
+  /// This function generates the DD4hep geometry
   StatusCode buildDD4HepGeo();
   // This function generates the Geant4 geometry
   StatusCode buildGeant4Geo();
   // receive DD4hep Geometry
-  virtual dd4hep::DetElement getDD4HepGeo();
-  virtual dd4hep::Detector* getDetector();
-  virtual const dd4hep::Detector* getDetector() const;
-  virtual std::string constantAsString(std::string const& name);
-  virtual std::string constantAsString(std::string const& name) const;
+  virtual dd4hep::DetElement getDD4HepGeo() override;
+  virtual const dd4hep::Detector* getDetector() const override;
+  virtual std::string constantAsString(std::string const& name) const override;
   // receive Geant4 Geometry
-  virtual G4VUserDetectorConstruction* getGeant4Geo();
+  virtual G4VUserDetectorConstruction* getGeant4Geo() override;
 
 private:
-  // Pointer to the interface to the DD4hep geometry
-  dd4hep::Detector* m_dd4hepgeo{nullptr};
-  // Pointer to the detector construction of DDG4
-  std::shared_ptr<G4VUserDetectorConstruction> m_geant4geo{nullptr};
+  /// Pointer to the interface to the DD4hep geometry
+  dd4hep::Detector* m_dd4hepgeo = nullptr;
+  /// Pointer to the detector construction of DDG4
+  std::shared_ptr<G4VUserDetectorConstruction> m_geant4geo;
 
   Gaudi::Property<std::vector<std::string>> m_xmlFileNames{this, "detectors", {}, "Detector descriptions XML-files"};
   Gaudi::Property<std::map<std::string, std::string>> m_sensitive_types{
