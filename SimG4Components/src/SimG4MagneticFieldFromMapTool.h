36f03bbeca8c161a393d2e6aeaf278f782058b4d
sss <sss@karma>
2026-04-03 15:52:10 -0400

    k4SimGeant4: Avoid G4SystemOfUnits.hh.
    G4SystemOfUnits.hh brings the names of all units into the global namespace.
    This pollution of common names (like s and m) can then result in -Wshadow
    warnings from clang.
    
    Use CLHEP/Units/SystemOfUnits.h instead, with explicit namespace
    qualifications where needed.

--- SimG4Components/src/SimG4MagneticFieldFromMapTool.h
+++ SimG4Components/src/SimG4MagneticFieldFromMapTool.h
@@ -9,7 +9,7 @@
 
 // Geant4
 #include "G4MagneticField.hh"
-#include "G4SystemOfUnits.hh"
+#include "CLHEP/Units/SystemOfUnits.h"
 
 // Forward declarations:
 // Geant 4 classes
@@ -70,9 +70,9 @@ private:
   /// Set with property DeltaOneStep
   Gaudi::Property<double> m_deltaOneStep{this, "DeltaOneStep", 0, "Delta(one-step)"};
   /// Upper limit of the step size, see G4 doc for more details. Set with property MaximumStep
-  Gaudi::Property<double> m_maxStep{this, "MaximumStep", 1. * m, "Maximum step length in field (see G4 documentation)"};
+  Gaudi::Property<double> m_maxStep{this, "MaximumStep", 1. * CLHEP::m, "Maximum step length in field (see G4 documentation)"};
   /// Lower limit of the step size, see G4 doc for more details. Set with property MinimumStep
-  Gaudi::Property<double> m_minStep{this, "MinimumStep", 0.01 * mm,
+  Gaudi::Property<double> m_minStep{this, "MinimumStep", 0.01 * CLHEP::mm,
                                     "Minimum step length in field (see G4 documentation)"};
   /// Name of the integration stepper, defaults to NystromRK4.
   Gaudi::Property<std::string> m_integratorStepper{this, "IntegratorStepper", "NystromRK4", "Integrator stepper name"};
